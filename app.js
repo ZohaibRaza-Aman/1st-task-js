49c60217b5584a1f774fd3e02d773751d376a791
zohaibraza <zohaibraza510@gmail.com>
2022-10-31 10:33:41 -0700

    work in process

---
+++ app.js
@@ -0,0 +1,141 @@
+// =========    1st    ==========
+
+alert ( "Thanks for your input!" );
+
+// =========    2st    ==========
+
+var myName = "Mark ";
+
+document.write(myName);
+
+//   ========== over write var ==========
+
+var nationality = "U.S";
+
+var nationality = "PK";
+
+document.write(nationality);
+
+// =========assign value in dual code========
+
+var lessonAuthor = "Mark";
+var guyWhoKeepsSayingHisOwnName = "Mark";
+var x = "Mark";
+
+var lastName = "Smith";
+var cityOfOrigin = "New Orleans";
+var aussieGreeting = "g'Day";
+
+// ==========     3rd       =========
+
+
+// ===========  add two var ==========
+
+var ab = 150;
+var bc = + 25;
+console.log(ab+bc)
+
+// ===========  add two var ==========
+
+var originalnum = 23;
+var newnum = originalnum + 7;
+console.log(newnum);
+
+// ==========  concate two var ============
+
+var originalNum = "23";
+var numToBeAdded = "7"; 
+var newNumbr = originalNum + numToBeAdded;
+console.log(newNumbr);
+
+// =====A variable can be used in calculating its own new value======
+
+var originalNum = 90;
+originalNum = originalNum + 10;
+console.log(originalNum)
+
+// ==========  concate two var ============
+
+var originalNum = "23";
+var newNum = originalNum + 7;
+console.log(newNum)
+
+// ==========   4th   ==========
+
+//  =======   var legal and illegal   ========
+
+// var abc  //legal
+// var 123  //illegal
+// var ab12 //legal
+// var 12ab //illegal
+// var var  //illegal
+// var @abc //illigal
+// var abc@ //illigal
+// var a+b //illegal
+// var a_b //lagal
+
+// ==========     5th    =========
+
+alert (2 + 2)
+
+var popularNumber = 12 - 24;
+document.write( popularNumber )
+
+var popularNumber = 3 * 12;
+console.log(popularNumber)
+
+var num = 10;
+var popularNumber = num + 100;
+console.log(popularNumber)
+
+var num = 10;
+var anotherNum = 1;
+var popularNumber = num + anotherNum;
+console.log(popularNumber)
+
+var whatsLeftOver = 10 % 3;
+
+console.log(whatsLeftOver)
+
+var whatsLeftOver = 9 % 3;
+
+console.log(whatsLeftOver)
+
+// =======   6th    ========
+
+
+
+// a++;
+// ++a;
+// a--;
+// --a;
+// +-a;
+
+var num = 1;
+var a = num++;
+
+console.log(a)
+
+var num = 1;
+var a = ++num;
+
+console.log(a);
+
+var num = 1;
+var a = num--;
+
+console.log(a);
+
+// =========   7th  =========
+
+var cost = 1 + 3 * 4;
+console.log(cost)
+
+var cost = (1 + 3) * 4;
+console.log(cost)
+
+// ========   8th  ========
+
+// alert("4" + "2");
+
+alert(4 + 2);
